6d8074bfb6f5013d68c2ff90b31fd34fc659fc37
endroid <info@endroid.nl>
2012-08-09 15:04:14 -0400

    Use MultiCurl when number of registration ID's exceeds 1000

--- src/Endroid/Gcm/Gcm.php
+++ src/Endroid/Gcm/Gcm.php
@@ -4,6 +4,7 @@ namespace Endroid\Gcm;
 
 use Buzz\Browser;
 use Buzz\Client\Curl;
+use Buzz\Client\MultiCurl;
 
 class Gcm
 {
@@ -16,6 +17,16 @@ class Gcm
      * @var string
      */
     protected $apiKey;
+
+    /**
+     * @var string
+     */
+    protected $registrationIdMaxCount = 1000;
+
+    /**
+     * @var array
+     */
+    protected $responses;
 
     /**
      * Class constructor
@@ -32,6 +43,13 @@ class Gcm
         }
     }
 
+    /**
+     * Sends the data to the given registration ID's via the GCM server
+     *
+     * @param $data
+     * @param $registrationIds
+     * @return \Buzz\Response
+     */
     public function send($data, $registrationIds)
     {
         $headers = array(
@@ -40,13 +58,40 @@ class Gcm
         );
 
         $data = array(
-            'registration_ids' => $registrationIds,
             'data' => $data,
         );
 
-        $buzz = new Browser(new Curl());
-        $response = $buzz->post($this->apiUrl, $headers, json_encode($data));
+        // Chunk number of registration ID's according to maximum allowed by GCM
+        $chunks = array_chunk($registrationIds, $this->registrationIdMaxCount);
 
-        return $response;
+        // Create the suitable client and browser
+        if (count($chunks) > 1) {
+            $client = new MultiCurl();
+        } else {
+            $client = new Curl();
+        }
+        $buzz = new Browser($client);
+
+        // Perform or enqueue the GCM request(s)
+        $this->responses = array();
+        foreach ($chunks as $registrationIds) {
+            $data['registration_ids'] = $registrationIds;
+            $this->responses[] = $buzz->post($this->apiUrl, $headers, json_encode($data));
+        }
+
+        // In case of MultiCurl: flush all GCM requests
+        if ($client instanceof MultiCurl) {
+            $client->flush();
+        }
+
+        // Determine success
+        foreach ($this->responses as $response) {
+            $message = json_decode($response->getContent());
+            if ($message === null || $message->success == 0 || $message->failure > 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
