3653d14c53ca912265de41fc81a968a8271e0d31
Baptiste M. <bactisme@frandroid.com>
2013-09-17 15:47:26 +0200

    Update Gcm.php
    Enhanced to provide to the end developer an clue about errors (CannonicalsIds etc).

--- src/Endroid/Gcm/Gcm.php
+++ src/Endroid/Gcm/Gcm.php
@@ -1,9 +1,16 @@
 <?php
 
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Endroid\Gcm;
 
 use Buzz\Browser;
-use Buzz\Client\Curl;
+use Buzz\Client\MultiCurl;
 
 class Gcm
 {
@@ -33,10 +40,22 @@ class Gcm
     protected $responses;
 
     /**
+     * Sent registration ids 
+     * @var array
+     */
+    protected $registrationIds = array();
+
+    /**
+     * Associatives array for id => errors
+     * @var array
+     */
+    protected $registrationErrors;
+
+    /**
      * Class constructor
      *
      * @param $apiKey
-     * @param null $baseUrl
+     * @param null $apiUrl
      */
     public function __construct($apiKey, $apiUrl = null)
     {
@@ -46,7 +65,7 @@ class Gcm
             $this->apiUrl = $apiUrl;
         }
 
-        $this->browser = new Browser(new Curl());
+        $this->browser = new Browser(new MultiCurl());
     }
 
     /**
@@ -59,6 +78,8 @@ class Gcm
      */
     public function send($data, array $registrationIds, array $options = array())
     {
+        $this->registrationIds = $registrationIds;
+        
         $headers = array(
             'Authorization: key='.$this->apiKey,
             'Content-Type: application/json'
@@ -77,7 +98,7 @@ class Gcm
             $data['registration_ids'] = $registrationIds;
             $this->responses[] = $this->browser->post($this->apiUrl, $headers, json_encode($data));
         }
-        //$this->browser->getClient()->flush();
+        $this->browser->getClient()->flush();
 
         // Determine success
         foreach ($this->responses as $response) {
@@ -88,6 +109,29 @@ class Gcm
         }
 
         return true;
+    }
+    
+    /**
+     * Return Ids associated with errors
+     *
+     * @return array RegistrationIds => Error
+     */
+    public function getRegistrationIdsAssociatedResponse(){
+        $this->registrationErrors = array();
+        $i = 0;
+        foreach ($this->responses as $response) {
+            $message = json_decode($response->getContent());
+            if ($message !== null && isset($message->results)){
+                foreach($message->results as $result){
+                    if (isset($this->registrationIds[$i]) && isset($result->error)){
+                        $device_id = $this->registrationIds[$i];
+                        $this->registrationErrors[$device_id] = $result->error;
+                        $i++;
+                    }
+                }
+            }
+        }
+        return $this->registrationErrors;
     }
 
     /**
