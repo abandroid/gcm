f89493454af658b2bcfcf29aef674ac86a380bf5
Omid Khadem <o.khadem@karafone.ir>
2016-10-26 12:55:37 +0330

    fixed an issue with notification type

--- src/Client.php
+++ src/Client.php
@@ -77,9 +77,7 @@ class Client
     {
         $this->responses = array();
 
-        $data = array_merge($options, array(
-            'data' => $data,
-        ));
+        $data = !empty($data) ? array_merge($options, array('data' => $data)) : array();
 
         if (isset($options['to'])) {
             $this->responses[] = $this->browser->post($this->apiUrl, $this->getHeaders(), json_encode($data));
